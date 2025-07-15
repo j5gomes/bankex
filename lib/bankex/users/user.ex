0def4481be5544ebcfe4f170314f119fe6a64f5c
João Gomes <joaogomes@significa.co>
2025-07-15 14:49:35 +0100

    cryptograph password

--- lib/bankex/users/user.ex
+++ lib/bankex/users/user.ex
@@ -1,11 +1,15 @@
 defmodule Bankex.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Argon2
 
-  @required_params [:name, :password_hash, :email, :nif]
+  alias Ecto.Changeset
+
+  @required_params [:name, :password, :email, :nif]
 
   schema "users" do
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :email, :string
     field :nif, :string
@@ -20,5 +24,12 @@ defmodule Bankex.Users.User do
     |> validate_length(:name, min: 3)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:nif, min: 9)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, hash_pwd_salt(password))
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Bankex.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:argon2_elixir, "~> 4.0"}
     ]
   end
 
